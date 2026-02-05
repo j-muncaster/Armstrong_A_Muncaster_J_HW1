0753c39b790dc719b5050dad5eb59913b39169f7
Jo Muncaster <j_muncaster208947@fanshaweonline.ca>
2026-02-05 17:53:01 -0500

    Updated the main.js and index.html
    In this commit, changes were made to the main.js and index.html files.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (() => {
+<<<<<<< Updated upstream
     // add loading icon(s) for list and content
 
     //for homework source the images for star wars API
@@ -67,3 +68,54 @@
         getCharacters();
     })();
     
+=======
+    
+   // Variables
+    const baseUrl = "https://swapi.info/api/";
+    const reviewCon = document.querySelector("#review-con");
+    const characterTemplate = document.querySelector("#character-template");
+
+
+    // Functions
+    function getCharacters(){
+        fetch(`${baseUrl}people`)
+        .then((res) => res.json())
+        .then((characters) => {
+            characters.forEach(character => {
+            console.log(character.name);
+            console.log(character.films[0]);
+
+            //create a ul, li, a, add data attribute to anchor tag that contains a film
+        });
+    })
+    
+      .then(()=> {
+        //attach an event listener to each link, calls a new function that makes 
+        // 2nd AJAX call
+        //function name is getMovie()
+      })
+      .catch((error) => {
+        console.error("Oops… this page has gone to a galaxy far, far away.")}
+        );
+    }
+    
+    function getMovie() {
+        fetch("https://swapi.info/api/films")
+            .then((res) => res.json())
+            .then((movie) => {
+                console.log(`img.src="images/poster${movie.episode_id}.jpg"`);
+                console.log(movie.title);
+                console.log(movie.opening_crawl);
+            })
+    
+            .catch((error) => {
+                console.error("Oops… this page has gone to a galaxy far, far away.")}
+                );
+        }
+    
+    // Calling the Functions
+    getCharacters();
+    getMovie();  
+
+})();
+>>>>>>> Stashed changes
